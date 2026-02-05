6867a902e22d4a20d8f18e2fb5bd451cb24890ac
mageshaudviklabs <magesh@audviklabs.com>
2026-02-05 12:28:02 +0530

    tailwind setup and layout folder

---
+++ src/components/topbar.tsx
@@ -0,0 +1,10 @@
+const TopBar = () => {
+  return (
+    <header className="h-14 bg-white border-b flex items-center justify-between px-6">
+      <h2 className="font-semibold">Manager Dashboard</h2>
+      <div className="text-sm text-gray-600"> Profile</div>
+    </header>
+  );
+};
+
+export default TopBar;
